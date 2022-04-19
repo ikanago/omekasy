31d4eb80474f7300fcd381cbf15111ce3b35b8ff
ikanago <ikanago-dev@protonmail.com>
2022-04-19 11:32:15 +0900

    Rough impl of interactive mode

--- src/convert.rs
+++ src/convert.rs
@@ -1,12 +1,12 @@
 use crate::font::Font;
 
-pub fn convert(source: &str, font: Font) -> String {
+pub fn convert(source: &[char], font: Font) -> String {
     let mapping = font.characters();
     source
-        .chars()
+        .iter()
         .map(|original| {
             if let Some(converted) = mapping.get(&original) {
-                *converted
+                converted
             } else {
                 original
             }
@@ -20,19 +20,23 @@ mod tests {
 
     #[test]
     fn skip_non_target_chars() {
-        let source = "ernct_jahmlsz あwgdqi-uxfpvobyk";
+        let source = "ernct_jahmlsz あwgdqi-uxfpvobyk"
+            .chars()
+            .collect::<Vec<_>>();
         assert_eq!(
             "𝐞𝐫𝐧𝐜𝐭_𝐣𝐚𝐡𝐦𝐥𝐬𝐳 あ𝐰𝐠𝐝𝐪𝐢-𝐮𝐱𝐟𝐩𝐯𝐨𝐛𝐲𝐤",
-            convert(source, Font::MathBold)
+            convert(&source, Font::MathBold)
         );
     }
 
     #[test]
     fn mathbold() {
-        let source = "8WymXbLV3nINUhOoQkKGfuY9HsZSC675jzBEtATDFMRgPpeaxiJcr0q4l1w2dv";
+        let source = "8WymXbLV3nINUhOoQkKGfuY9HsZSC675jzBEtATDFMRgPpeaxiJcr0q4l1w2dv"
+            .chars()
+            .collect::<Vec<_>>();
         assert_eq!(
             "𝟖𝐖𝐲𝐦𝐗𝐛𝐋𝐕𝟑𝐧𝐈𝐍𝐔𝐡𝐎𝐨𝐐𝐤𝐊𝐆𝐟𝐮𝐘𝟗𝐇𝐬𝐙𝐒𝐂𝟔𝟕𝟓𝐣𝐳𝐁𝐄𝐭𝐀𝐓𝐃𝐅𝐌𝐑𝐠𝐏𝐩𝐞𝐚𝐱𝐢𝐉𝐜𝐫𝟎𝐪𝟒𝐥𝟏𝐰𝟐𝐝𝐯",
-            convert(source, Font::MathBold)
+            convert(&source, Font::MathBold)
         );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::io::{self, Write};
+
 use clap::Parser;
 use font::Font;
+use termion::{cursor, event::Key, input::TermRead, raw::IntoRawMode, clear};
 
 use crate::convert::convert;
 
@@ -18,7 +21,37 @@ struct Cli {
 }
 
 fn main() {
-    let cli = Cli::parse();
+    // let cli = Cli::parse();
 
-    println!("{}", convert(&cli.source, cli.font));
+    // println!("{}", convert(&cli.source, cli.font));
+
+    let stdin = io::stdin();
+    let mut stdin = stdin.lock().keys();
+    let stdout = io::stdout();
+    let stdout = stdout.lock();
+    let mut stdout = stdout.into_raw_mode().unwrap();
+
+    let mut source = Vec::new();
+
+    loop {
+        let b = stdin.next().unwrap().unwrap();
+        match b {
+            Key::Backspace => {
+                write!(stdout, "{}{}{}", cursor::Left(1), clear::AfterCursor, cursor::Save).unwrap();
+                source.pop();
+            }
+            Key::Char('q') => {
+                break;
+            }
+            Key::Char(c) => {
+                write!(stdout, "{}{}", c, cursor::Save).unwrap();
+                source.push(c);
+            }
+            _ => {}
+        }
+        write!(stdout, "\r\n{}", clear::AfterCursor).unwrap();
+        write!(stdout, "{}", convert(&source, Font::MathBold)).unwrap();
+        write!(stdout, "{}", cursor::Restore).unwrap();
+        stdout.flush().unwrap();
+    }
 }
